64be260ecbabf2765b164955736c41ba5f2f373c
michalHC <1554675+michalHC@users.noreply.github.com>
2023-04-06 01:58:47 +0200

    minor fixes + soapUI project

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Sample spring soap service
 
 based on https://www.concretepage.com/spring-boot/spring-boot-soap-web-service-example 
 adjusted for employee table layout
+
+
+db run in docker:
+```
+docker run --name mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d -p 3306:3306 mysql:latest
+```
+
+get into container:
+```
+docker exec -it mysql /bin/bash
+```
--- db-setup.sql
+++ db-setup.sql
@@ -2,16 +2,14 @@ CREATE DATABASE IF NOT EXISTS `hr`;
 USE `hr`;
 
 CREATE TABLE IF NOT EXISTS `employees` (
-  `employee_id` bigint(5) NOT NULL AUTO_INCREMENT,
+  `employee_id` bigint(5) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email_address` varchar(80),
   `job_id` varchar(5),
   `department_id` varchar(5),
-  `salary` varchar(10),
-
-  PRIMARY KEY (`employee_id`)
-) ENGINE=InnoDB;
+  `salary` varchar(10)
+);
 
 INSERT INTO `employees` (`employee_id`, `first_name`, `last_name`, `salary`) VALUES
 	(1, 'Tom', 'The Cat', 500),
